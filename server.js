62ad17de2c937ca26b397b892593685f4f268ea2
gabriella-omah <gabriellaomenye@gmail.com>
2026-07-30 07:55:49 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Serve CSS, JS, images, HTML, etc.
+app.use(express.static(__dirname));
+
+// Home = landing page (first visit)
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "landing.html"));
+});
+
+// Other pages
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/workouts", (req, res) => {
+    res.sendFile(path.join(__dirname, "workouts.html"));
+});
+
+app.get("/workout", (req, res) => {
+    res.sendFile(path.join(__dirname, "workout.html"));
+});
+
+app.get("/library", (req, res) => {
+    res.sendFile(path.join(__dirname, "library.html"));
+});
+
+app.get("/progress", (req, res) => {
+    res.sendFile(path.join(__dirname, "progress.html"));
+});
+
+app.get("/settings", (req, res) => {
+    res.sendFile(path.join(__dirname, "settings.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`LiftLog running at http://localhost:${PORT}`);
+});
